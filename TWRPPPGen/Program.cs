f16b7b9d18f39783192701d330978c983fb191d3
usrDottik <dotcomnose@gmail.com>
2022-01-19 18:46:11 -0300

    add some initial code, added tool that will be used in the project in the credits

--- TWRPPPGen/Program.cs
+++ TWRPPPGen/Program.cs
@@ -2,9 +2,30 @@
 {
     public class Program 
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Program's Main Entry Point!
+        /// </summary>
+        /// <param name="args">Arguments given at startup.</param>
+        public static void Main(string[] args)
         {
+            bool recoveryImg = false, 
+                 bootImg = false;
+            //Parse args.
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].ToLower().Contains("--recovery_image"))
+                {
+                    recoveryImg = true;
+                } 
+                else if (args[i].ToLower().Contains("--boot_image"))
+                {
+                    bootImg = true;
+                }
+            }
+            if (recoveryImg)
+            {
 
+            }
         }
     }
 }
