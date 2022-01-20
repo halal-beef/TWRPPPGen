6abfe5210885485a57f4e85d4ad8f32c0bb1e49e
usrDottik <dotcomnose@gmail.com>
2022-01-20 19:14:43 -0300

    Add prop parser(WIP)

--- TWRPPPGen/Main Operations/ProcessInvoker.cs
+++ TWRPPPGen/Main Operations/ProcessInvoker.cs
@@ -14,12 +14,12 @@
     internal class ProcessInvoker
     {
         /// <summary>
-        /// Invoke a System Shell and use it to run a Batch script, application or anything that can be executed!
+        /// Invoke a System Shell and use it to run a Batch script, application or anything that can be executed! (Made to run AIK.)
         /// </summary>
         /// <param name="PathToFile">Path to the batch or executable</param>
         /// <param name="shouldRedirectOutput">should redirect Normal output? </param>
         /// <returns>CmdOut containing result data.</returns>
-        public static CmdOut InvokeCMD(string PathToFile, string arguments, bool shouldRedirectOutput)
+        public static CmdOut InvokeCMD(string PathToFile, string arguments, bool shouldRedirectOutput, bool isAIK = false)
         {
             CmdOut cOut = new();
             Process proc = new();
@@ -29,13 +29,36 @@
             sinfo.Arguments = arguments;
             sinfo.WindowStyle = ProcessWindowStyle.Hidden;
             sinfo.CreateNoWindow = true;
-            sinfo.UseShellExecute = true;
+            sinfo.UseShellExecute = false;
             
-            if(shouldRedirectOutput)
+            if (shouldRedirectOutput)
                 sinfo.RedirectStandardOutput = true;
+
+            Thread x = new(
+            () => {
+                StreamReader sr = proc.StandardOutput;
+                while (true)
+                {
+                    //Avoid pointing to something null.
+                    string a = ". ";
+                    a += sr.ReadLine();
+                    if (a.Contains("Done"))
+                    {
+                        Thread.Sleep(1000);
+                        proc.Kill();
+                        break;
+                    }
+                }
+            });
 
             proc.StartInfo = sinfo;
             proc.Start();
+
+            //Wait some time...
+            Thread.Sleep(2 * 1000);
+            if (isAIK)
+                x.Start();
+
             proc.WaitForExit();
 
             if (shouldRedirectOutput)
---
+++ TWRPPPGen/Main Operations/PropParser.cs
@@ -0,0 +1,35 @@
+﻿namespace TWRPPPGen
+{
+    internal class PropParser
+    {
+        /// <summary>
+        /// Parses a default.prop file, and returns all the properties on it as a List
+        /// </summary>
+        /// <returns>List of strings containing the properties</returns>
+        public static List<string> ParseFile(string PathToPropFile)
+        {
+            List<string> parsed = new();
+
+            string[] fileLines = File.ReadAllLines(PathToPropFile);
+            
+            //Remove comments from file.
+            for (int i = 0; i < fileLines.Length; i++)
+            {
+                if (!fileLines[i].Contains('#'))
+                {
+                    StringBuilder prop = new();
+                    prop.Append(fileLines[i]);
+
+                    parsed.Add(prop.ToString());
+                }
+                else
+                {
+                    parsed.Add("\n");
+                }
+            }
+
+            //Return raw data.
+            return parsed;
+        }
+    }
+}
