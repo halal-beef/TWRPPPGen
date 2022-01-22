50d926c04f7fd66afda52b68d942642aedefc5d5
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2022-01-22 13:41:22 +0000

    Update MakeFile.cs

--- TWRPPPGen/Main Operations/MakeFile.cs
+++ TWRPPPGen/Main Operations/MakeFile.cs
@@ -7,6 +7,8 @@
         /// </summary>
         public static void CopyFiles(string targetFolder, string hware)
         {
+            if(Data.CurrentOS.Equals(OSPlatform.Windows))
+            {
             // List all .rc files in the extracted Ramdisk root folder.
             string[] dotRCFiles = Directory.GetFiles(Data.PathToAIK + @"\ramdisk\", "*.rc");
 
@@ -52,6 +54,56 @@
             else if (File.Exists(Data.PathToAIK + @"\split_img\recovery.img-kernel"))
             {
                 File.Copy(Data.PathToAIK + @"\split_img\recovery.img-kernel", targetFolder + @"\prebuilt\zImage", true);
+            }
+          }
+            else if(Data.CurrentOS.Equals(OSPlatform.Linux))
+            {
+                            // List all .rc files in the extracted Ramdisk root folder.
+            string[] dotRCFiles = Directory.GetFiles(Environment.CurrentDirectory + @"/Android Image Kitchen" + @"/ramdisk/", "*.rc");
+
+            for (int i = 0; i < dotRCFiles.Length; i++)
+            {
+                string[] part2 = dotRCFiles[i].Split('//');
+
+                // Copy the file if it maches.
+                if (part2.Last().Contains($"init.recovery.{hware}")
+                 || part2.Last().Contains($"init.recovery.{hware}")
+                 || part2.Last().Contains($"ueventd.{hware}")
+                 || part2.Last().Contains($"ueventd.{hware}"))
+                {
+                    File.Copy(dotRCFiles[i], targetFolder + @"/recovery/root/" + part2.Last(), true);
+                }
+            }
+
+            // NO SAR
+            if (Directory.Exists(Data.PathToAIK + @"/ramdisk/system/"))
+            {
+                if (Directory.Exists(Data.PathToAIK + @"/ramdisk/system/etc"))
+                {
+                    if (File.Exists(Data.PathToAIK + @"/ramdisk/system/etc/recovery.fstab"))
+                    {
+                        File.Copy(Data.PathToAIK + @"/ramdisk/system/etc/recovery.fstab", targetFolder + @"/recovery/root/system/etc/recovery.fstab", true);
+                    }
+                }
+            } 
+            // SAR
+            else if (Directory.Exists(Data.PathToAIK + @"/ramdisk/etc/"))
+            {
+                if (File.Exists(Data.PathToAIK + @"/ramdisk/etc/recovery.fstab"))
+                {
+                    File.Copy(Data.PathToAIK + @"/ramdisk/etc/recovery.fstab", targetFolder + @"/recovery/root/etc/recovery.fstab", true);
+                }
+            }
+
+            // Copy kernel -> zImage
+            if (File.Exists(Data.PathToAIK + @"/split_img/boot.img-kernel"))
+            {
+                File.Copy(Data.PathToAIK + @"/split_img/boot.img-kernel", targetFolder + @"/prebuilt/zImage", true);
+            }
+            else if (File.Exists(Data.PathToAIK + @"/split_img/recovery.img-kernel"))
+            {
+                File.Copy(Data.PathToAIK + @"/split_img/recovery.img-kernel", targetFolder + @"/prebuilt/zImage", true);
+            }
             }
         }
     }
